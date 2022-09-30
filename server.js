829ce1858c0cb926912a2281a59da6e94764926b
JENNA PARKER <github-actions[bot]@users.noreply.github.com>
2022-09-30 15:39:05 -0400

    requires dependencies, middleware and initializes listener

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+///////////////////
+// DEPENDENCIES //
+/////////////////
 
+require("dotenv").config();
+const { PORT = 4000 } = process.env;
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const app = express();
+
+/////////////////
+// MIDDLEWARE //
+///////////////
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+///////////////cd 
+// LISTENER //
+/////////////
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`))
