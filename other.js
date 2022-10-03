71811f88e2c7eb2366fcf4c50d772684a53d236a
JENNA PARKER <github-actions[bot]@users.noreply.github.com>
2022-10-03 12:42:29 -0400

    converst other.json to other.js and exports data

---
+++ other.js
@@ -0,0 +1,179 @@
+module.exports=
+[
+    {
+        chemical: "Propylene glycol",
+        description: "When used in high doses or for prolonged periods, PG toxicity can occur. Reported adverse effects from PG include central nervous system (CNS) toxicity, hyperosmolarity, hemolysis, cardiac arrhythmia, seizures, agitation, and lactic acidosis",
+        products: [
+            {
+                name: "Colgate toothpaste",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGmublrMpqsS8Ju4Q4dzJ_FHtTWmWBYDg-mw&usqp=CAU"
+            },
+            {
+                name: "Listerene",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6SAmc_rcN5wwSZDST8cdaRNFN9h5K_vxsOw&usqp=CAU"
+            },
+            {
+                name: "Sun Bum tan lotion ",
+                img: "https://s3.amazonaws.com/images.ecwid.com/images/27159231/1459852348.jpg"
+            },
+            {
+                name: "Mario Badescu Facial Serum",
+                img: "https://sweetievulgar.com/wp-content/uploads/s2185387-main-zoom.jpg"
+            }
+        ]
+    },
+    {
+        chemical: "Phthalates",
+        description: "Has been linked asthma, ADHD, breast cancer, obesity, autism spectrum disorders, altered reproductive development, male fertility issues and many more problems. ",
+        products: [
+            {
+                name: "Dove deodorant",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxELKw5DoegDRiSjrE5MsTUBagu9WJBp6CP6uZRL9pgKkZL4uA7ckGmerWHHEX2zJyDKI&usqp=CAU"
+            },
+            {
+                name: "Nivea Creme",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF97PfXeZcjHf5dJ8SKXodrZDTmSQnq7VVbQ&usqp=CAU"
+            },
+            {
+                name: "Vaseline",
+                img: "https://content.instructables.com/ORIG/FRR/9BLX/IGTY6DGY/FRR9BLXIGTY6DGY.jpg?auto=webp&fit=bounds&frame=1&height=1024&width=1024auto=webp&frame=1&height=150"
+            },
+            {
+                name: "Huggies Baby wipes",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQofrAoPf4sxJaUD4Tfduk2GNj0Gn7EXuJOqqw379k8ZKi3OJS0nF9r3vS5_Xazt_h5lBk&usqp=CAU"
+            }
+        ]
+    },
+    {
+        chemical: "Formaldehyde",
+        description: "Ingestion of as little as 30 mL (1 oz.) of a solution containing 37% formaldehyde has been reported to cause death in an adult. Ingestion may cause corrosive injury to the gastrointestinal mucosa, with nausea, vomiting, pain, bleeding, and perforation. Corrosive injuries are usually most pronounced in the pharyngeal mucosa, epiglottis and esophagus. Systemic effects include metabolic acidosis, CNS depression and coma, respiratory distress, and renal failure.",
+        "products": [
+            {
+                name: "Estee Lauder Lipstick",
+                img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTEbtQQp6o_XPq3AsNeLwfnY8oRXCCVt6gqcftRIZrr5WZFH5aASjHjsw85X069MeUFViRfOPp8&usqp=CAE"
+            },
+            {
+                name: "Revlon Hair dye",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISKSkPZwJEizW4bahf40Sjxz23T0zpv7ApQ&usqp=CAU"
+            },
+            {
+                name: "O.P.I Nail Polish",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXMH14YYM4JcrokOrdktiN58eMqm3JfHTYdg&usqp=CAU"
+            },
+            {
+                name: "Ruthys Textile Cotton Sheet Set",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNIDqTOiWrAQ-AQwJ698oReVLlElHQ8DkVug&usqp=CAU"
+            }
+        ]
+    },
+    {
+        chemical: "Parabens",
+        description: "This popular cosmetic preservative mimics estrogen, potentially causing breast cancer and interfering with male reproduction. It is also known to increase skin aging (something most beauty products claim to reverse) and DNA damage.",
+        "products": [
+            {
+                name: "Olay hydrating moisturizer",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR29Pgl19hp10FpVr6CRvX7YlA4xedpvuPzw&usqp=CAU"
+            },
+            {
+                name: "Burts Bees lip balm",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM8WYvdaovpIo_1ECI8MJaT4UXJrUZYiomOA&usqp=CAU"
+            }
+        ]
+    },
+    {
+        chemical: "Arsenic",
+        description: "found in the microchips of many electronic devices including mobile phones. In high doses, arsenic poisoning is lethal. Low levels of exposure cause negative impacts on skin, liver, nervous and respiratory systems.",
+        products: [
+            {
+                name: "Apple iPhones",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJW-tt5U2Po4L_gytW1cz1Uq9X_XtVs2ZjfQ&usqp=CAU"
+            },
+            {
+                name: "Roundup",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxAfc9cRndUWuNgpzrF0lvY5RIpxrFSdkFcQ&usqp=CAU"
+            }
+        ]
+    },
+    {
+        chemical: "Bromine",
+        description: "A component in a group of fire retardant chemicals known as brominated flame retardants. It is associated with cognitive and developmental deficits. Studies have shown that bromine contributes to the disruption in the thyroid hormone balance, brain damage and cancer.",
+        "products": [
+            {
+                name: "Android Cell Phones",
+                img: "https://tse3.mm.bing.net/th?id=OIP.hC8Jipg2pV6BY-ZYHL4FuQHaHa&pid=Api&P=0"
+            },
+            {
+                name: "Purell hand sanitizer",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMLMS1KTuKzQEu0iZvrvsgpvNOFF8XYe7s9Q&usqp=CAU"
+            }
+        ]
+    },
+    {
+        chemical: "Toluene",
+        description: "Symptoms of toluene poisoning include CNS effects (headache, dizziness, ataxia, drowsiness, euphoria, hallucinations, tremor, seizures, and coma), ventricular arrythmias, chemical pneumonitis, respiratory depression, nausea, vomiting, and electrolyte imbalances. There is no antidote for toluene.",
+        products: [
+            {
+                name: "Glade Scented Candles",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQULvEKjv2PUIowtPt4Bm1q8cOWInz3la-NdQ&usqp=CAU"
+            },
+            {
+                name: "Newport cigarettes",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd_rVnBwoDzNFr6L1IL3h1vlRWiVfrZDK4iQ&usqp=CAU"
+            }
+        ]
+    },
+    {
+        chemical: "Paraffin",
+        description: "Eating a lot of paraffin can lead to intestinal obstruction, which can cause abdominal pain, nausea, vomiting, and possible constipation.",
+        products: [
+            {
+                name: "CeraVe Moisturizing Cream",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqpBU-X2BCB-JseUn9KyIxADpq5_WUVbfcpA&usqp=CAU"
+            },
+            {
+                name: "Nair Hair Removal",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_QOf-GCYMroMgDXdzt-9bPjrapXNt7uLIWg&usqp=CAU"
+            },
+            {
+                name: "Swan Mineral Oil",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTrAlJi-aP080WQOXJHWmNh5hZLnJ47LDnCw&usqp=CAU"
+            }
+        ]
+    },
+    {
+        "chemical": "Bisphenol (BPA) ",
+        "description": "Exposure to BPA is a concern because of the possible health effects on the brain and prostate gland of fetuses, infants and children. It can also affect children's behavior. ",
+        "products": [
+            {
+                "name": "Tampax Tampons",
+                "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWzoQWeQKbfLnxzPdUNZiTwfjlVPOpp5Lo1penuFKWTp5mDr5yI4ONRynZ6YjY1NlmhA8&usqp=CAU"
+            },
+            {
+                "name": "Lens Crafters Glasses",
+                "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwVWj7cuRvtWuHUM6ORuCwbMPMZGWI5KK5iw&usqp=CAU"
+            },
+            {
+                "name": "Dr.Browns Baby Bottles",
+                "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTie8rSbQLG3NvOv7e41a5o11Pv2rT5e0zaiw&usqp=CAU"
+            }
+        ]
+    },
+    {
+        "chemical": "Triclosan",
+        "description": "Triclosan is an ingredient that helps prevent or reduce bacterial contamination. Short-term studies show links to a decrease in thyroid hormones",
+        "products": [
+            {
+                "name": "Dial Body Wash",
+                "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlXoQS22oKt9W92mWmvZcao0ziF4N11WsoXcnCh-1Qig-Ssh4jrWZpSsWVm9CiNyB4wW8&usqp=CAU"
+            },
+            {
+                "name": "Dawn Dishwashing Liquid",
+                "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGRa--sAek3zsHf4ooyNb7Xh8eCvDgbOwTnA&usqp=CAU"
+            },
+            {
+                "name": "Bath & Body Works hand soap",
+                "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg22m9IuydvOtSQ2mXtoxR905y-LaQ4q2prA&usqp=CAU"
+            }
+        ]
+    }
+]
